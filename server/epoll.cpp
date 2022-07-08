da7f336d5cd2a5003a20436829e62bd2b2bfa289
zeroet <seyun@e1r3p21.clusters.42paris.fr>
2022-07-08 21:56:35 +0200

    epoll

--- server/epoll.cpp
+++ server/epoll.cpp
@@ -1,1 +1,37 @@
 #include "epoll.hpp"
+
+Epoll::Epoll() {}
+
+Epoll::Epoll(std::vector<Block> block) : vecBloc_(block)
+{
+    create_epoll_fd();
+}
+
+Epoll::Epoll(const Epoll &other) : vecBloc_(other.vecBloc_), mapClnt_(other.mapClnt_), epollFd_(other.epollFd_)
+{
+    *this = other;
+}
+
+Epoll::~Epoll() { }
+
+void    Epoll::create_epoll_fd()
+{
+    this->epollFd_ = epoll_create(MAX_EVENT);
+    if (epollFd_ < 0)
+    { 
+        close_all_serv_socket();
+        // tmp exit error
+        std::cout << "Epoll fail" << std::endl;
+        exit(1);
+    }
+}
+
+void    Epoll::close_all_serv_socket()
+{
+    int count = vecBloc_.size();
+    
+    for(int i = 0; i < count; i++)
+        close(vecBloc_[i].getter_socketFd());
+    close(this->epollFd_);
+    std::cout << "all socket closed" << std::endl;
+}
--- server/epoll.hpp
+++ server/epoll.hpp
@@ -1,9 +1,33 @@
-#include <iostream>
-#include <string.h>
-#include <fcntl.h>
-#include <vector>
-
-#include <sys/unistd.h>
-#include <sys/socket.h>
+#include "socket.hpp"
 #include <sys/epoll.h>
-#include <arpa/inet.h>
+#include <map>
+
+# define MAX_EVENT 1024
+# define TIMEOUT -1
+
+class Request
+{
+    Request();
+};
+
+class Epoll
+{
+
+public:
+    typedef int                         clntFd;
+    typedef std::vector<Block>          vecBloc;
+    typedef std::map<clntFd, Request>   mapClnt;
+    typedef struct epoll_event          event;
+
+private:
+    vecBloc vecBloc_;
+    mapClnt mapClnt_;
+    event   ev_[MAX_EVENT];
+    int     epollFd_;
+
+public:
+    Epoll();
+    Epoll(std::vector<Block> block); // epoll fd create
+    Epoll(const Epoll &other);
+    ~Epoll();
+};
