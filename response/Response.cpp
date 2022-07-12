652c457aa50cbaf0d9c8ead9d5053577bea552ce
eyoo <eyoo@Eunmiui-MacBookPro.local>
2022-07-12 19:16:00 +0200

    response code+message complete

--- response/Response.cpp
+++ response/Response.cpp
@@ -1,22 +1,81 @@
-#include "response.hpp"
+#include "Response.hpp"
 
-Response::Response(std::string status)
+void		Response::map_make_pair(void)
 {
-	this->status_code = status.substr(0, 3);
-	// get 3 string(number) in the status.
-	this->status_message = status.substr(4);
-	//from index 4 to end
-	//should be added values in the status.(ex: 200 ok);
+	mapStatus_.insert(std::make_pair("100", "Continue"));
+	mapStatus_.insert(std::make_pair("101", "Switching Protocols"));
+	mapStatus_.insert(std::make_pair("103", "Early Hints"));
+	mapStatus_.insert(std::make_pair("200", "OK"));
+	mapStatus_.insert(std::make_pair("201", "Created"));
+	mapStatus_.insert(std::make_pair("202", "Accepted"));
+	mapStatus_.insert(std::make_pair("203", "Non-Authoritative Information"));
+	mapStatus_.insert(std::make_pair("204", "No Content"));
+	mapStatus_.insert(std::make_pair("205", "Reset Content"));
+	mapStatus_.insert(std::make_pair("206", "Partial Content"));
+	mapStatus_.insert(std::make_pair("300", "Multiple Choices"));
+	mapStatus_.insert(std::make_pair("301", "Moved Permanently"));
+	mapStatus_.insert(std::make_pair("302", "Found"));
+	mapStatus_.insert(std::make_pair("303", "See Other"));
+	mapStatus_.insert(std::make_pair("304", "Not Modified"));
+	mapStatus_.insert(std::make_pair("305", "Use Proxy"));
+	mapStatus_.insert(std::make_pair("306", "unsued"));
+	mapStatus_.insert(std::make_pair("307", "Temporary Redirect"));
+	mapStatus_.insert(std::make_pair("308", "Permanent Redirect"));
+	mapStatus_.insert(std::make_pair("400", "Bad Request"));
+	mapStatus_.insert(std::make_pair("401", "Unauthorized"));
+	mapStatus_.insert(std::make_pair("402", "Payment Required"));
+	mapStatus_.insert(std::make_pair("403", "Forbidden"));
+	mapStatus_.insert(std::make_pair("404", "Not Found"));
+	mapStatus_.insert(std::make_pair("405", "Method Not Allowed"));	
+	mapStatus_.insert(std::make_pair("408", "Request Timeout"));	
+	mapStatus_.insert(std::make_pair("411", "Length Required"));	
+	mapStatus_.insert(std::make_pair("413", "Payload Too Large"));	
+	mapStatus_.insert(std::make_pair("414", "URI Too Long"));	
+	mapStatus_.insert(std::make_pair("500", "Internal Server Error"));	
+	mapStatus_.insert(std::make_pair("502", "Bad Gateway"));	
+	mapStatus_.insert(std::make_pair("505", "HTTP Version Not Supported"));	
+
+	std::map<std::string, std::string>::iterator it;
+	it = this->mapStatus_.find("100");
+	this->status_code = it->first;
+	this->status_message = it->second;	
+	std::cout << status_code << std::endl;
+	std::cout << status_message << std::endl;
+}
+		
+
+Response::Response()
+{
 }
 
-Response::~Response();
+
+Response::Response(const std::string &status)
+{
+	std::cout << this->status_code << std::endl;
+	std::cout << this->status_message << std::endl;
+	map_make_pair();
+	response_manager();
+}
+
+
+Response::~Response()
 {
 }
+
+ void	Response::response_manager()
+ {
+ 	std::string str;
+
+ 	str = makeHeader();
+ 	std::cout << str << std::endl;
+ 	makeStatusBody();
+ 	std::cout << body << std::endl;
+ }
 
 void Response::appendHeader(std::string first, std::string second)
 {
 	mHeaders.insert(std::make_pair(first, second));
-{
+}
 
 void Response::setBody(std::string & str)
 {
@@ -27,7 +86,7 @@ std::string Response::makeHeader()
 {
 	std::string outcome;
 	
-	outcome.append("HTTP/1.1 " + status_code + " " + status_message + "\r\n");
+	outcome.append("HTTP/1.1 " + this->status_code + " " + this->status_message + "\r\n");
 	for (std::map<std::string, std::string>::iterator it = mHeaders.begin(); it != mHeaders.end(); it++)
 		outcome.append((*it).first + ": " + (*it).second + "\r\n");
 	outcome.append("\r\n");
@@ -43,17 +102,17 @@ void Response::makeStatusBody()
 	outcome.append("<h1>" + status_code + "</h1>");
 	outcome.append("<h3>" + status_message + "/h3>");
 	outcome.append("<p>Click <a href=\"/\">here</a> to return home.</p>");
-	outcome.append("</body></html>");
+	outcome.append("</body></html>\r\n\r\n");
 	
 	body.clear();
-	body = outcome();
+	body = outcome;
 }
 
 void Response::makeStatusBody(std::string url)
 {
 	std::string outcome;
 	outcome.append(url);
-	body.clear;
+	body.clear();
 	body = outcome;
 }
 
--- response/Response.hpp
+++ response/Response.hpp
@@ -7,23 +7,30 @@
 
 class Response
 {
-	private:
-		std::map<std::string. std::string> mHeaders;
 	public:
 		Response();
-		Response(std::string status);
+		Response(const std::string &status);
 		~Response();
+	
+		typedef std::map<std::string, std::string> mapStatus;	
 		
+		std::string 	makeHeader();
+		void			appendHeader(std::string first, std::string second);
+		void			setBody(std::string & str);
+		void			makeStatusBody();
+		void			makeStatusBody(std::string url);
+		int				getBodySize();
+		void	map_make_pair();
+		void	response_manager();
+
+	private:
+		std::map<std::string, std::string> mHeaders;
 		std::string status_code;
 		std::string status_message;
 		std::string body;
-		std::string makeHeader();
+		mapStatus mapStatus_;
 
-		void	appendHeader(std::string first, std::string second);
-		void	setBody(std::string & str);
-		void	makeStatusBody();
-		void	makeStatusBody(std::string url);
-		int	getBodySize();
+		
 	};
 
 #endif
