cad8fd483dd3b6dd2cbc63f8f5287efbb55e2d84
Keungeun LEE <klee8903@gmail.com>
2022-07-16 18:32:46 +0200

    request ing with buffer

--- request/main.cpp
+++ request/main.cpp
@@ -3,24 +3,44 @@
 #include <sstream>
 #include <string>
 #include <istream>
+#include "../server/epoll.hpp"
+// #include "../server/socket.hpp"
 #include "Request.hpp"
 
 int	main(int ac, char** av) {
-	if (ac != 2)
-	{
-		std::cout << "parse exemple needed" << std::endl;
-		return (1);
-	}
-	else
-	{
-		std::ifstream file(av[1]);
-		std::string line;
-		while (std::getline(file, line))
-		{
-			std::cout << line << std::endl;
-			if (line.find("\r\n\r\n") != (std::string::npos))
-				std::cout << "fin" << std::endl;
-		}
-	}
+
+	(void)av;
+	(void)ac;
+
+	std::vector<Block> server;
+    Block a(8080);
+    Block b(8000);
+    Block c(7000);
+    server.push_back(a);
+    server.push_back(b);
+    server.push_back(c);
+
+    Socket socket(server);
+    std::vector<Block> test = socket.getter_vecBloc();
+
+    std::cout << "Server Fd : " << test[0].getter_socketFd() << std::endl;
+    std::cout << "Server Port : " << test[0].getter_portNumber() << std::endl;
+
+    std::cout << "Server Fd : " << test[1].getter_socketFd() << std::endl;
+    std::cout << "Server Port : " << test[1].getter_portNumber() << std::endl;
+
+    std::cout << "Server Fd : " << test[2].getter_socketFd() << std::endl;
+    std::cout << "Server Port : " << test[2].getter_portNumber() << std::endl;
+
+	// if (ac != 2)
+	// {
+	// 	std::cout << "parse exemple needed" << std::endl;
+	// 	return (1);
+	// }
+	// else
+	// {
+
+
+	// }
 	return (0);
 }
