d0c10a32984ea685016a470102a2b0371cd1ec78
keungeun Lee <klee8903@gmail.com>
2022-08-25 02:27:13 +0200

    File tree when autoindex on done for first draft

--- server/connection.cpp
+++ server/connection.cpp
@@ -121,9 +121,14 @@ void    Connection::processResponse()
 		if (currentMethod_ == "GET" || currentMethod_ == "POST") {
 				if (isGetHTML) {
 					if (autoindex_flag) {
-						std::cout << "HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE" << std::endl;
-						body_ = response_.bodyWithAutoindexOn(request_.getPath(), request_.getFilePath());
-						req_status_code_ = 200;
+						struct stat			fileinfo;
+						stat(request_.getFilePath().c_str(), &fileinfo);
+						if (S_ISDIR(fileinfo.st_mode))
+						{
+						// std::cout << "HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE" << std::endl;
+							body_ = response_.bodyWithAutoindexOn(request_.getPath(), request_.getFilePath());
+							req_status_code_ = 200;
+						}
 					}
 					else
 					{
@@ -164,9 +169,13 @@ void    Connection::processResponse()
 	send(clntFd_, const_cast<char*>(returnBuffer_.c_str()), returnBuffer_.size(), 0);
 
 	status_ = "Close";
+
+	
 	// epollout, close fd
 	//ep_->epoll_Ctl_Mode(clntFd_, EPOLLIN);
 	
+	//clear everything before circulate again.
+	clear();
 }
 
 //getter
@@ -205,10 +214,10 @@ LocationBlock	Connection::getLocationConfig(void) {
 std::string		&Connection::getBodyBuf(void) {
 	// il faut faire protection pour content-length
 
-	std::istringstream		contentLength(request_.getHeaderValue("Content-Length"));
-	int						contentLength_;
-	contentLength >> contentLength_;
-	body_buf.resize(contentLength_);
+	// std::istringstream		contentLength(request_.getHeaderValue("Content-Length"));
+	// int						contentLength_;
+	// contentLength >> contentLength_;
+	// body_buf.resize(contentLength_);
 	
 	return (body_buf);
 }
