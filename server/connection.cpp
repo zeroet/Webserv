8cd56fa927ad92714c5592bb13bb9a1c2a1e8cd2
hyungyoo <hjyoo901112@gmail.com>
2022-08-26 03:22:30 +0200

    corriger connection.cpp epollout condition

--- server/connection.cpp
+++ server/connection.cpp
@@ -73,7 +73,7 @@ void    Connection::processRequest(void) {
 		if (phase_msg_ == BODY_COMPLETE)
 		{
 			std::cout << "************ Message body process **********" << std::endl;
-//			size_t pos = 0;
+			size_t pos = 0;
 			if (getRequest().getMethod() == "GET" || getRequest().getMethod() == "DELETE")
 			{
 				if (buffer_.empty())
@@ -85,7 +85,8 @@ void    Connection::processRequest(void) {
 			else
 			{
 				request_.setBody(getBodyBuf());
-//				if ((pos = buffer_.find(CRLFCRLF)) != std::string::npos)
+				if ((pos = buffer_.find(CRLFCRLF)) != std::string::npos
+					||  (size_t)buffer_content_length == body_buf.size())
 					ep_->epoll_Ctl_Mode(clntFd_, EPOLLOUT);
 			}
 		}
